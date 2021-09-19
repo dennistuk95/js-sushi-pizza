7510eac80b2beca14eeb1b0b07cef2c134a64447
dennistuk95 <88230815+dennistuk95@users.noreply.github.com>
2021-09-19 10:35:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var makePizza = function () {
+    console.log("Prepare the ingredients");
+    console.log("Take the dough");
+    console.log("Put tomato sauce on the dough");
+    console.log("Put all other ingredients on");
+    console.log("Put the pizza in the oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
+
+
+var makeSushi = function () {
+    console.log("Prepare the ingredients");
+    console.log("Take the rice");
+    console.log("Put salmon in the rice");
+    console.log("Enjoy");
+};
+
+makeSushi();
+makeSushi();
